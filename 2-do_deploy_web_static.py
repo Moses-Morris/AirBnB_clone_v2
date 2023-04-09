f04ee36755a4ffcdd50bfd21bff966b76f3a1f2d
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-09 19:04:37 +0000

    Configure and use fab file to send files to remote server using fab

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,73 +1,47 @@
 #!/usr/bin/python3
-"""
-	Generating a tgz file with the contents of a web static folder
-"""
-
-import os
+"""The code Generates a .tgz archive from the contents of the web_static folder."""
 from fabric.api import *
-from datetime import datetime
+import time
+import os
 
 env.hosts = ["54.152.135.110", "54.237.14.152"]
+env.user = "ubuntu"
+
 
 def do_pack():
     """
-    this is an archive of the contents in web static folder
+    Generate a .tgz archive from web_static folder. and store to a versions folder
     """
-    current = datetime.now()
-    fileArchive = "versions/web_static_{}{}{}{}{}{}.tgz".format(current.year,
-                                                                current.month,
-                                                                current.day,
-                                                                current.hour,
-                                                                current.minute,
-                                                                current.second)
-    print("Packing web_static to {}".format(fileArchive))
-    """creation of the directory versions
-    """
-    local("mkdir -p versions")
-    result = local("tar -cvzf {} web_static".format(fileArchive))
-    if result.succeeded:
-        return (fileArchive)
-    else:
+    try:
+        local("mkdir -p versions")
+        file_name = "web_static_{}.tgz".format(
+            time.strftime("%Y%m%d%H%M%S"))
+        file_path = "versions/{}".format(file_name)
+        local("tar -cvzf {} web_static/".format(file_path))
+        return file_path
+    except:
         return None
 
-    """
-    these creates and distributes an  archive to your web servers
-    """
-
 def do_deploy(archive_path):
-    """deploying the archive to the web servers
     """
-    name_path = archive_path.split("/")[1]
-    if not os.path.exists(archive_path):
-        return False
+        Distribute archive to your remote server locations. The env.hosts.
+    """
+    if os.path.exists(archive_path): #This checks if the path exists throught the operating system.
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
-    result_path = put(archive_path, "/tmp/")
-    if result_path.failed:
-        return False
+        print("New version deployed!")
+        return True
 
-    run("mkdir -p /data/web_static/releases/{}".format(name_path[:-4]))
-
-    cmd = "tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(name_path,
-                                                                     name_path[:-4])
-    result_path = run(cmd)
-    if result_path.failed:
-        return False
-
-    result_path = run("rm /tmp/{}".format(name_path))
-    if result_path.failed:
-        return False
-
-    run("cp -rp /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(name_path[:-4], name_path[:-4]))
-
-    run("rm -rf /data/web_static/releases/{}/web_static/".format(name_path[:-4]))
-    result_path = run("rm /data/web_static/current")
-    if result_path.failed:
-        return False
-
-    path_uncompress = "/data/web_static/releases/{}".format(name_path[:-4])
-    cmd = "ln -sf {} /data/web_static/current".format(path_uncompress)
-    result_path = run(cmd)
-    if result_path.failed:
-        return False
-    return True
+    return False
