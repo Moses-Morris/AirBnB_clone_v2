9069939561777706d4bba5a464d459161b253cec
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-22 14:12:35 +0000

    Create a Flask app with a return statement.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+	Flask web application
+	Web Application should be listening at 0.0.0.0 to allow other users on the network
+	to discover and access the application on port 5000
+	Routes: '/' (Default entry index of App)
+	Route Option : strict_slashes=False
+"""
+
+from flask import Flask
+
+""" Instance of Flask App """
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	return "Hello HBNB"
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
