270977667da6ed4d65f5238c3d5a3d5f0f651c5f
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-22 20:04:31 +0000

    Fix : State.py.

--- models/state.py
+++ models/state.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+"""Defines the State class."""
+import models
 from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
 from models.city import City
-from models import storage
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """Represents a State"""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Return the list of City instances with
-            state_id equals to the current State.id
-            """
-            values_city = storage.all(City).values()
-            list_city = []
-            for city in values_city:
+            """Get a list of all related Cities."""
+            city_l = []
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
-                    list_city.append(city)
-            return list_city
+                    city_l.append(city)
+            return city_l
