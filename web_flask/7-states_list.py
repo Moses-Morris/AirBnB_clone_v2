79a79a60a34663909da498585f42c1315cca62c8
Moses-Morris <mosesmorrisdev@gmail.com>
2023-04-22 18:52:37 +0000

    List all state objects in a storage engine.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,59 @@
 #!/usr/bin/python3
-"""
-    Runs a Flask web application on 0.0.0.0:5000
-"""
-from ../models import storage
-from flask import Flask
-from flask import render_template
+# script that starts a Flask web application
+
+from models import *
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """ Returns an HTML page of all States sorted by name """
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
+@app.route('/')
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<string:s>')
+def c(s):
+    new_s = s.replace("_", " ")
+    return "C {}".format(new_s)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:s>')
+def python(s="is cool"):
+    new_s = s.replace("_", " ")
+    return "Python {}".format(new_s)
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', num=n)
+
+
+@app.route('/states_list')
+def states_list():
+    return render_template('7-states_list.html',
+                           states=storage.all("State"))
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """ Removes the current SQLAlchemy session. """
+def teardown(err):
     storage.close()
 
 
 if __name__ == "__main__":
-    """ Run on 0.0.0.0 """
     app.run(host='0.0.0.0')
